24bc0cb91d77ae2dd1f01096399caf2ccca761c1
WiSmKo <william@thewhisk.dev>
2026-04-25 23:16:20 +0200

    prune bloat

--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,18 @@
-import { Suspense } from 'react'
-
+import { Suspense } from "react";
 import Header from "@/components/Header";
-
-import Hero from "@/components/Hero";
-
-import SpotifyEmbed from '@/components/SpotifyEmbed';
-
 import Footer from "@/components/Footer";
-
-import RecordSearchForm from '@/components/RecordSearchForm';
-
+import RecordSearchForm from "@/components/RecordSearchForm";
 
 export default function Home() {
-
   return (
-
     <>
-
       <Suspense>
-
         <Header />
-
       </Suspense>
-
       <main>
-
-        <Hero />
-
         <RecordSearchForm />
-
-        <SpotifyEmbed />
-
       </main>
-
       <Footer />
-
     </>
-
   );
-
 }
--- next.config.js
+++ next.config.js
@@ -2,14 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: [
-      // NextJS <Image> component needs to whitelist domains for src={}
-      "lh3.googleusercontent.com",
-      "pbs.twimg.com",
-      "images.unsplash.com",
-      "logos-world.net",
-      'i.discogs.com'
-    ],
+    domains: ["i.discogs.com"],
   },
 };
 
